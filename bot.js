cdd45f0a70754470d0b66d5ffadcc22837c53d2c
younesstn <57918235+younesstn@users.noreply.github.com>
2020-04-29 21:01:23 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,17 @@ client.on('message', message => {
   	}
 });
 
+
+client.on("message", message => {
+  if(message.content.startsWith("!banner")) {
+    if(message.guild.bannerURL === null || message.guild.bannerURL === undefined) return message.channel.send("his server doesn't have a banner.");
+    const ba = new Discord.RichEmbed()
+    .setAuthor(message.guild.name, message.guild.iconURL)
+    .setDescription(`[Banner URL](${message.guild.bannerURL}?size=2048)`)
+    .setImage(message.guild.bannerURL + "?size=2048")
+    message.channel.send({embed : ba})
+  }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
